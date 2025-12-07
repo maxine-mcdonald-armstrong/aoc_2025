8a6bdc4f21f5f6dee7704abae707da36cae31aed
Maxine McDonald Armstrong <maxine.armstrong2005@gmail.com>
2025-12-07 13:14:31 +0000

    clang-format

--- d1/1.cpp
+++ d1/1.cpp
@@ -4,22 +4,21 @@ using ll = long long;
 using ull = unsigned long long;
 
 int main() {
-  std::ios::sync_with_stdio(false);
-  std::cin.tie(nullptr);
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
 
-  std::fstream fp("in.txt", std::ios_base::in);
-  int cur = 50;
-  int ans = 0;
-  char c;
-  int x;
-  while (fp >> c >> x) {
-    if (c == 'L')
-      cur -= x;
-    else
-      cur += x;
-    cur = (cur % 100 + 100) % 100;
-    if (cur == 0)
-      ans++;
-  }
-  std::cout << ans << std::endl;
+    std::fstream fp("in.txt", std::ios_base::in);
+    int cur = 50;
+    int ans = 0;
+    char c;
+    int x;
+    while (fp >> c >> x) {
+        if (c == 'L')
+            cur -= x;
+        else
+            cur += x;
+        cur = (cur % 100 + 100) % 100;
+        if (cur == 0) ans++;
+    }
+    std::cout << ans << std::endl;
 }
--- d1/2.cpp
+++ d1/2.cpp
@@ -4,23 +4,22 @@ using ll = long long;
 using ull = unsigned long long;
 
 int main() {
-  std::ios::sync_with_stdio(false);
-  std::cin.tie(nullptr);
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
 
-  std::fstream fp("in.txt", std::ios_base::in);
-  int cur = 50;
-  int ans = 0;
-  char c;
-  int x;
-  while (fp >> c >> x) {
-    for (int i = 0; i < x; i++) {
-      if (c == 'L')
-        cur--;
-      else
-        cur++;
-      if (cur % 100 == 0)
-        ans++;
+    std::fstream fp("in.txt", std::ios_base::in);
+    int cur = 50;
+    int ans = 0;
+    char c;
+    int x;
+    while (fp >> c >> x) {
+        for (int i = 0; i < x; i++) {
+            if (c == 'L')
+                cur--;
+            else
+                cur++;
+            if (cur % 100 == 0) ans++;
+        }
     }
-  }
-  std::cout << ans << std::endl;
+    std::cout << ans << std::endl;
 }
